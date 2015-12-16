5bd095f582e4c826ee223094119e9abd8eb8e339
sun <1752709589@qq.com>
2015-12-16 16:05:18 +0800

    heater analysis

--- app/models/vanke_device_usage.rb
+++ app/models/vanke_device_usage.rb
@@ -1,4 +1,35 @@
 class VankeDeviceUsage < ActiveRecord::Base
+
+  # 热水器使用房间数-by day
+  def self.heater_usage_by_day(start_time, end_time)
+    result = {}
+    # vanke_heater_ids = VankeDeviceHouseTable.where("device_type='bulb'").map(&:device_id)
+    records = VankeDeviceUsage.where(created_at: Date.parse(start_time)..Date.parse(end_time)).where(device_type: 'heater')
+    records.each do |item|
+      tmp = item.created_at.to_s[0..9]
+      result[tmp] = [] if result[tmp].nil?
+      result[tmp] << item.device_id unless result[tmp].include?(item.device_id)
+    end
+    result.each{|k,v| result[k]=v.size}
+    result.sort.to_h
+  end
+
+  # 热水器使用概览
+  def self.heater_usage_overview(start_time, end_time)
+    result = {}
+    vanke_heater_ids = VankeDeviceHouseTable.where("device_type='bulb'").map(&:device_id)
+    records = VankeDeviceUsage.where(created_at: Date.parse(start_time)..Date.parse(end_time)).where(device_type: 'heater')
+    records.each do |item|
+      result[item.device_id] = [] if result[item.device_id].nil?
+      tmp = item.created_at.to_s[0..9]
+      result[item.device_id] << tmp unless result[item.device_id].include?(tmp)
+    end
+    a = ["未使用",'0',(vanke_heater_ids-result.keys).size,((vanke_heater_ids-result.keys).size.to_f/vanke_heater_ids.size).round(2)]
+    b = ["普通使用",'1-9天',(result.select{|k,v| v.size < 10}).size,((result.select{|k,v| v.size < 10}).size.to_f/vanke_heater_ids.size).round(2)]
+    c = ["高频使用",'>10天',(result.select{|k,v| v.size >= 10}).size,((result.select{|k,v| v.size >= 10}).size.to_f/vanke_heater_ids.size).round(2)]
+    [a,b,c]
+  end
+
   # zgrep switch_o access.log.[1-5].gz >> vanke_heater.log.2015-12-14
   # VankeDeviceUsage.fetch_heater_data('2015-12-01', '2015-12-30')
   def self.fetch_heater_data(start_time, end_time, vanke_only = true)
