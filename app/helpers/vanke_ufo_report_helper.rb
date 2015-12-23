fe88405af60879112f83a3508f65d3a81d03fd98
sun <1752709589@qq.com>
2015-12-23 17:21:52 +0800

    vanke ufo report

--- app/helpers/vanke_ufo_report_helper.rb
+++ app/helpers/vanke_ufo_report_helper.rb
@@ -6,6 +6,14 @@ module VankeUfoReportHelper
       end
       @everyday_lighting_hour[k] = @everyday_lighting_hour[k].to_f
     end
-    [{name: "每天累计照明时间", data: @everyday_lighting_hour}]
+    [{name: "每天累计照明时间(单位:小时)", data: @everyday_lighting_hour}]
+  end
+  def get_everyday_hour_linghing_hour(date)
+    data = @everyhour_lighting_hour.select{|k,v| k.include?(date)}.inject({}){|hash,(k,v)| hash.merge(k[11..13]+"时"=>v)}
+    [{name: "#{date}照明时长分布(单位:秒)", data: data}]
+  end
+  def get_everyday_hour_linghing_on(date)
+    data = @everyhour_lighting_on.select{|k,v| k.include?(date)}.inject({}){|hash,(k,v)| hash.merge(k[11..13]+"时"=>v)}
+    [{name: "#{date}累计开启次数分布(单位:次)", data: data}]
   end
 end
