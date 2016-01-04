9b2879eed6d62d7f6e6106e794aac9b7e1c09dee
sun <1752709589@qq.com>
2016-01-04 18:33:28 +0800

    change

--- app/controllers/vanke_report_controller.rb
+++ app/controllers/vanke_report_controller.rb
@@ -1,12 +1,17 @@
 class VankeReportController < ApplicationController
   def index
-    @heater_usage_overview = VankeDeviceUsage.device_usage_overview('heater','2015-11-08','2015-12-12')
-    @ecotower_usage_overview = VankeDeviceUsage.device_usage_overview('ecotower','2015-11-08','2015-12-12')
-    @heater_usage_by_day = VankeDeviceUsage.device_usage_by_day('heater','2015-11-08','2015-12-12')
-    @ecotower_usage_by_day = VankeDeviceUsage.device_usage_by_day('ecotower','2015-11-08','2015-12-12')
-    @door_accesses_usage_by_day = VankeDeviceUsage.door_accesses_usage_by_day('2015-11-08','2015-12-12')
-    @door_accesses_usage_by_hour = VankeDeviceUsage.door_accesses_usage_by_hour('2015-11-08','2015-12-12')
-    @heater_usage_by_hour = VankeDeviceUsage.device_usage_by_hour('heater','2015-11-08','2015-12-12')
-    @ecotower_usage_by_hour = VankeDeviceUsage.device_usage_by_hour('ecotower','2015-11-08','2015-12-12')
+    @start_time = '2015-11-08'
+    @end_time = '2015-12-12'
+    @heater_usage_overview = VankeDeviceUsage.device_usage_overview('heater',@start_time,@end_time)
+    @ecotower_usage_overview = VankeDeviceUsage.device_usage_overview('ecotower',@start_time,@end_time)
+    @heater_usage_by_day = VankeDeviceUsage.device_usage_by_day('heater',@start_time,@end_time)
+    @ecotower_usage_by_day = VankeDeviceUsage.device_usage_by_day('ecotower',@start_time,@end_time)
+    @door_accesses_usage_by_day = VankeDeviceUsage.door_accesses_usage_by_day(@start_time,@end_time)
+    @door_accesses_usage_by_hour = VankeDeviceUsage.door_accesses_usage_by_hour(@start_time,@end_time)
+    @heater_usage_by_hour = VankeDeviceUsage.device_usage_by_hour('heater',@start_time,@end_time)
+    @ecotower_usage_by_hour = VankeDeviceUsage.device_usage_by_hour('ecotower',@start_time,@end_time)
+    @heater_usage_by_control_type = VankeDeviceUsage.device_usage_through_app_switch('heater',@start_time,@end_time)
+    @ecotower_usage_by_control_type = VankeDeviceUsage.device_usage_through_app_switch('ecotower',@start_time,@end_time)
+    # binding.pry
   end
 end
