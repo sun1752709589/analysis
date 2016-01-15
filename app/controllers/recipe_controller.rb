a88b5118f13f5c872beab8857b3c5d06f7a07f6f
sun <1752709589@qq.com>
2016-01-15 13:17:58 +0800

    change

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -2,7 +2,7 @@ class RecipeController < ApplicationController
   def index
     @recipes = Recipe.all
     @recipe_overview = Recipe.per_recipe_overview
-    @recipe_create_by_day = Recipe.per_recipe_by_day('2015-10-12','2015-11-13')
+    @recipe_create_by_day = Recipe.per_recipe_by_day('2015-12-01','2016-01-10')
     @user_recipes = Recipe.recipe_count_by_user
   end
 end
